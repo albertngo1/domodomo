bbeb319c4918f02c6f4f0cb1e87e29ea9493f3b9
Albert Ngo <albertngo1@gmail.com>
2017-10-31 03:33:45 -0400

    small apple bug fix

--- bundler.js
+++ bundler.js
@@ -275,8 +275,14 @@ class Apple {
   applePos() {
     const w = $l('#cv').nodes[0].width;
     const h = $l('#cv').nodes[0].height;
-    this.appleX = Math.round((Math.random()*w)/20)*20 - 20;
+    this.appleX = Math.round(Math.random()*w/20)*20 - 20;
     this.appleY = Math.round((Math.random()*h)/20)*20 - 20;
+    if (this.appleX < 0) {
+      this.appleX += 20;
+    }
+    if (this.appleY < 0) {
+      this.appleY += 20;
+    }
   }
 
   draw(ctx) {
